27c73715ee794bd8ac1eb2a84a04318d4b143076
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-05-08 20:16:33 +0300

    → Checkpoint at: 20:47:52

--- src/Kinoafisha/index.js
+++ src/Kinoafisha/index.js
@@ -64,6 +64,7 @@ export const Kinoafisha = () => {
                     </div>
                 </div>
             </div>
+            <div className = 'content'>{moviesJSX}</div>
         </>
     );
 };
