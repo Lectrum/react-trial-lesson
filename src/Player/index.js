5c3302ad983c4564868378ae9ed66f37895c41bc
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-07-02 20:29:49 +0300

    → Checkpoint at: 20:57:31

--- src/Player/index.js
+++ src/Player/index.js
@@ -58,6 +58,8 @@ export const Player = () => {
 
     const playControl = isPlaying ? <>&#10074;&#10074;</> : <>&#9654;</>;
 
+    console.log(progress)
+
     return (
         <div className = 'player'>
             <video
